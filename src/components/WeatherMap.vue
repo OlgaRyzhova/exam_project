2b0aaaa296edf4d3e76efa9919333523b6526780
Olga <miliukovao@gmail.com>
2022-11-06 00:17:08 +0200

    change link

--- src/components/WeatherMap.vue
+++ src/components/WeatherMap.vue
@@ -12,19 +12,26 @@
     <div class="container">
       <div class="top">
         <div class="location">
-          <p class="city_name">{{ weather.cityName }}</p>
-          <p class="country_name">{{ weather.country }}</p>
+          <p class="city_name">{{ weather.city.name }}</p>
+          <p class="country_name">{{ weather.city.country}}</p>
         </div>
         <div class="temp">
-          <h1>{{ weather.temperature }}&deg;C</h1>
+          <h1>{{ weather.temp }}&deg;C</h1>
         </div>
-        <div class="weather">
-          <p class="description">{{ weather.description }}</p>
+        <div class="description">
+          <p>{{ weather.description }}</p>
         </div>
       </div>
+      <div class="forecast">
+        <div class="daily">
+          <p class="bold">{{ weather.cnt }}</p>
+          <p>Forecast</p>
+        </div>
+      </div>
+      <!-- <weather-img-show :weather="weather" :lst-id="listId"/> -->
       <div class="bottom">
         <div class="feels">
-          <p class="bold">{{ weather.feelsLike }}&deg;C</p>
+          <p class="bold">{{ weather.feels_like }}&deg;C</p>
           <p>Feels Like</p>
         </div>
         <div class="humidity">
@@ -32,7 +39,7 @@
           <p>Humidity</p>
         </div>
         <div class="wind">
-          <p class="bold">{{weather.windSpeed}} MPH</p>
+          <p class="bold">{{weather.speed}} MPH</p>
           <p>Wind Speed</p>
         </div>
       </div>
@@ -50,7 +57,7 @@ export default {
     return {
       findLocation: "",
       weather: {
-        // cityName: "London",
+        // cityName: "",
         // country: "UK",
         // temperature: 12,
         // description: "Snow",
@@ -61,23 +68,28 @@ export default {
       api_key: "441c85811b1cad6112e54a6e91f88d32",
     };
   },
+  // components: {
+  //   weatherImgShow,
+  // }
   methods: {
     fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(
-          `https://api.openweathermap.org/data/2.5/weather?q=${this.findLocation}&units=metric&APPID=${this.api_key}`
+          `https://api.openweathermap.org/data/2.5/forecast?q=${this.findLocation}&cnt=10&units=metric&lang=en,ua&appid=${this.api_key}`
+          // https://api.openweathermap.org/data/2.5/weather?q=${this.findLocation}&cnt=10&units=metric&APPID=${this.api_key}`
         )
           .then((res) => {
             return res.json();
           })
           .then(this.setResults);
+          this.findLocation = ''
       }
     },
     setResults(results) {
       this.weather = results;
-      // this.weather.cityName = data.name;
-      // this.weather.country = data.sys.country;
-      // this.weather.temperature = Math.round(data.main.temp);
+      // this.weather.cityName = weather.name;
+      // this.weather.country = weather.sys.country;
+      // this.weather.temperature = weather.main.temp;
       // this.weather.description = data.weather[0].description;
       // this.weather.feelsLike = Math.round(data.main.feels_like);
       // this.weather.humidity = Math.round(data.main.humidity);
